23d2de145b335e74290e9fd20292164f3f74ad7e
Bruno Toledo <brunotoledo@me.com>
2025-06-11 22:39:31 -0300

    feat: supplier controller

--- src/ProtonSEC.Templates.ThreeLayers.WebAPI/Controllers/SuppliersController.cs
+++ src/ProtonSEC.Templates.ThreeLayers.WebAPI/Controllers/SuppliersController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProtonSEC.Templates.ThreeLayers.Business.Interfaces;
+using ProtonSEC.Templates.ThreeLayers.Business.Models;
 using ProtonSEC.Templates.ThreeLayers.WebAPI.ViewModels;
 
 namespace ProtonSEC.Templates.ThreeLayers.WebAPI.Controllers
@@ -7,21 +8,30 @@ namespace ProtonSEC.Templates.ThreeLayers.WebAPI.Controllers
 	[Route("api/suppliers")]
 	public class SuppliersController : MyControllerBase
 	{
-		public SuppliersController(INotifier notifier) : base(notifier)
-		{
+		private readonly ISupplierRepository _supplierRepository;
+		private readonly ISupplierService _supplierService;
 
+		public SuppliersController(ISupplierRepository supplierRepository, ISupplierService supplierService, INotifier notifier) : base(notifier)
+		{
+			_supplierRepository = supplierRepository;
+			_supplierService = supplierService;
 		}
 
 		[HttpGet]
 		public async Task<IEnumerable<SupplierViewModel>> GetAll()
 		{
-			throw new NotImplementedException();
+			return (await _supplierRepository.GetAllAsync()).Select(x => new SupplierViewModel(x));
 		}
 
 		[HttpGet("{id:guid}")]
-		public async Task<IEnumerable<SupplierViewModel>> GetById(Guid id)
+		public async Task<ActionResult<SupplierViewModel>> GetById(Guid id)
 		{
-			throw new NotImplementedException();
+			Supplier? supplier = await _supplierRepository.GetByIdAsync(id);
+
+			if (supplier == null)
+				return NotFound();
+
+			return new SupplierViewModel(supplier);
 		}
 
 		[HttpPost]
--- src/ProtonSEC.Templates.ThreeLayers.WebAPI/ViewModels/SupplierViewModel.cs
+++ src/ProtonSEC.Templates.ThreeLayers.WebAPI/ViewModels/SupplierViewModel.cs
@@ -3,38 +3,53 @@ using ProtonSEC.Templates.ThreeLayers.Business.Models;
 
 namespace ProtonSEC.Templates.ThreeLayers.WebAPI.ViewModels
 {
-    public class SupplierViewModel
-    {
-        public Guid Id { get; set; }
+	public class SupplierViewModel
+	{
+		public Guid Id { get; set; }
 
-        [Required]
-        [StringLength(100)]
-        public required string Name { get; set; }
+		[Required]
+		[StringLength(100)]
+		public string Name { get; set; }
 
-        [Required]
-        [StringLength(20)]
-        public required string Document { get; set; }
+		[Required]
+		[StringLength(20)]
+		public string Document { get; set; }
 
-        public int SupplierType { get; set; }
+		public int SupplierType { get; set; }
 
-        public bool Active { get; set; }
+		public bool Active { get; set; }
 
-        public AddressViewModel? Address { get; set; }
+		public AddressViewModel? Address { get; set; }
 
-        public IEnumerable<ProductViewModel> Products { get; set; } = new List<ProductViewModel>();
+		public IEnumerable<ProductViewModel> Products { get; set; } = new List<ProductViewModel>();
 
-        public Supplier ToSupplierEntity()
-        {
-            return new Supplier()
-            {
-                Id = Id,
-                Name = Name,
-                Document = Document,
-                SupplierType = (SupplierTypes)SupplierType,
-                Active = Active,
-                Address = null,
-                Products = Products.Select(p => p.ToEntity()).ToList()
-            };
-        }
-    }
+		public SupplierViewModel()
+		{
+			Name = Document = String.Empty;
+		}
+		
+		public SupplierViewModel(Supplier supplier)
+		{
+			Name = supplier.Name;
+			Document = supplier.Document;
+			SupplierType = (int)supplier.SupplierType;
+			Active = supplier.Active;
+			Address = null;
+			Products = [];
+		}
+
+		public Supplier ToSupplierEntity()
+		{
+			return new Supplier()
+			{
+				Id = Id,
+				Name = Name,
+				Document = Document,
+				SupplierType = (SupplierTypes)SupplierType,
+				Active = Active,
+				Address = null,
+				Products = Products.Select(p => p.ToEntity()).ToList()
+			};
+		}
+	}
 }
